f96a5db85ca7c6d602dfd4248670c6227ebcdc10
SteyDong <>
2021-01-15 10:35:16 +0800

    增加引用

--- JPCategory-OC/JPCategory.h
+++ JPCategory-OC/JPCategory.h
@@ -9,5 +9,12 @@
 #define JPCategory_h
 
 #import "JPCategory-UIButton.h"
+#import "NSObject+JPCategory.h"
+#import "NSArray+JPCategory.h"
+#import "NSBundle+JPCategory.h"
+#import "UIView+JPCategory.h"
+#import "UIImage+JPCategory.h"
+#import "UIColor+JPCategory.h"
+#import "UILabel+JPCategory.h"
 
 #endif /* JPCategory_h */
